26f3c7b155a9d37c02638b8bda2164ba5ec064f6
separeit894 <rinatramazanov894@gmail.com>
2026-08-07 07:02:35 +0300

    bump to version 0.0.5
    1. New opportunity `Получить файлы `zip``
    2. Add error handler

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
-from flask import Flask, request, render_template, send_file, redirect, url_for, abort
-import os
+from flask import Flask, request, render_template, send_file, redirect, url_for, abort, after_this_request
 from zipfile import ZipFile
+
+import os
+import time
+
 
 app = Flask(__name__)
 
+# Settings
+HOST="0.0.0.0"
+PORT="5000"
+
+
 UPLOADS_DIR = "uploads"
 ZIP_ARCHIVE_DIR = "zip_archive"
-zip_filename = "files.zip"
+ZIP_FILENAME = "files.zip"
+
+# if create zip, return file to download
+create_zip = False
+
+# Make dirs if not exists
 os.makedirs(ZIP_ARCHIVE_DIR, exist_ok=True)
 os.makedirs(UPLOADS_DIR, exist_ok=True)
 
-VERSION = "0.0.4"
+
+VERSION = "0.0.5"
 
 @app.route("/")
 def index():
     data = os.listdir(UPLOADS_DIR)
 
-    return render_template("index.html", data=data, version=f"version: {VERSION}", zipfile=zip_filename)
+    if os.path.exists(os.path.join(ZIP_ARCHIVE_DIR, ZIP_FILENAME)):
+        os.remove(os.path.join(ZIP_ARCHIVE_DIR, ZIP_FILENAME))
+
+    return render_template("index.html", data=data, version=f"version: {VERSION}", zipfile=ZIP_FILENAME)
 
 @app.route("/upload", methods=["POST", "GET"])
 def upload_file():
@@ -44,7 +61,8 @@ def delete_file(filename):
 
 @app.route("/create-zip-archive", methods=["POST"])
 def create_zip_archive():
-    zip_path = os.path.join(ZIP_ARCHIVE_DIR, zip_filename)
+    global create_zip
+    zip_path = os.path.join(ZIP_ARCHIVE_DIR, ZIP_FILENAME)
 
     if request.method == "POST":    
         result = request.get_json()
@@ -52,8 +70,9 @@ def create_zip_archive():
         if os.path.exists(zip_path):
             print(f"[ DELETE ] File exists: {zip_path}... Delete file")
             os.remove(zip_path)
-        
+        create_zip = False
         with ZipFile(zip_path, "w") as myzip:
+            print(result)
             for i in result:
                 src_path = os.path.join(UPLOADS_DIR, i)
                 if os.path.exists(src_path):
@@ -61,16 +80,36 @@ def create_zip_archive():
                 else:
                     print(f"File not founded : {src_path}")
             myzip.close()
+            create_zip = True
+            print(f"[ CREATE ZIP FILE ] : create_zip : {create_zip}")
 
-        return send_file(zip_path, as_attachment=False, download_name=zip_filename)
+        return send_file(zip_path, as_attachment=True, download_name=ZIP_FILENAME)
     else:
         return render_template("page_not_found.html")
 
 @app.route("/get-zip-archive/<string:zipfile>", methods=['GET', 'POST'])
 def get_zip_archive(zipfile):
-    import time
-    time.sleep(3)
-    return send_file(os.path.join(ZIP_ARCHIVE_DIR, zipfile), as_attachment=False, download_name=zip_filename)
+    global create_zip
+    while not create_zip:
+        time.sleep(2)
+        print(f"[ VARIABLE CREATE_ZIP ] : {create_zip}")
+        if create_zip:
+            break
+
+    zip_path = os.path.join(ZIP_ARCHIVE_DIR, zipfile)
+    @after_this_request
+    def delete_zip_file(response):
+        if os.path.exists(zip_path):
+            print(f"[ DELETE ZIPFILE IF EXISTS ] : {zip_path}")
+            os.remove(zip_path)
+
+        return response
+
+    return send_file(zip_path, as_attachment=False, download_name=ZIP_FILENAME)
+
+@app.errorhandler(404)
+def error_page(error):
+    return render_template("page_not_found.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host=HOST, port=PORT)
